9eafeeac39b2e3d622b652f08f2610c575653354
Sashika11 <113249340+Sashika11@users.noreply.github.com>
2025-12-31 16:25:54 +0530

    Update session_filter.py

--- core/session_filter.py
+++ core/session_filter.py
@@ -1,10 +1,13 @@
-from datetime import datetime
+from datetime import datetime, time as dtime
 import pytz
 
 def valid_session():
     tz = pytz.timezone('US/Eastern')
     now = datetime.now(tz).time()
-    if (now >= datetime.strptime('03:00','%H:%M').time() and now <= datetime.strptime('07:00','%H:%M').time()) or \
-       (now >= datetime.strptime('08:00','%H:%M').time() and now <= datetime.strptime('12:00','%H:%M').time()):
-        return True
-    return False
+
+    london_open = dtime(3,0)
+    london_close = dtime(7,0)
+    ny_open = dtime(8,0)
+    ny_close = dtime(12,0)
+
+    return (london_open <= now <= london_close) or (ny_open <= now <= ny_close)
